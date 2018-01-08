9745848938509e39c1f301de463f456b29b2595b
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-01-08 18:17:59 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,5 @@
 #write your code here
 
-def countdown(n)
-  x = n
-  while x > 0 do
-    puts "#{number} SECOND(S)!"
-    x -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
 
 def countdown_with_sleep(n)
   x = n
